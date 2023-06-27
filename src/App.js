0f32f232185b0aafd7155d7bf3e831a5e3052dfe
Cesar Garabito <cesargarabito97@gmail.com>
2023-06-27 19:38:40 +0200

    filtro por titulo y autor

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Main from './components/Main/Main';
 function App() {
   return (
     <div className="App">
-      <h1 style={{ color: "navy", textAlign: "left" }}>Podcast</h1>
+      <h1 style={{ color: "navy", textAlign: "left", marginBottom: '0' }}>Podcast</h1>
       <hr />
-      
+      <br></br>
       <Main />
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import PodcastCard from "../PodcastCard/PodcastCard";
-import './Main.css'
+import "./Main.css";
 
 function Main() {
   const [podcasts, setPodcasts] = useState([]);
+  const [filterText, setFilterText] = useState("");
 
   const fetchData = () => {
     fetch(
@@ -12,26 +13,44 @@ function Main() {
       .then((response) => response.json())
       .then((data) => setPodcasts(data.feed.entry));
   };
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value);
+  };
+  const filteredPodcasts = podcasts.filter((podcast) =>
+    podcast.title.label.toLowerCase().includes(filterText.toLowerCase()) ||
+    podcast["im:artist"].label.toLowerCase().includes(filterText.toLowerCase())
+  );
 
   useEffect(() => {
     fetchData();
-    
   }, []);
   return (
-    <div className="card-container">
-      {podcasts.map((podcast, index) => {
-        return (
-          //console.log(podcast['im:artist'].label)
-        <PodcastCard 
-        key={index}
-        image={podcast['im:image'][0].label}
-        title={podcast.title.label}
-        author={podcast['im:artist'].label}
+    <div>
+    <div className="filter-container">
+    <div className="filter-text">
+          {filteredPodcasts.length} 
+        </div>
+        <input
+          type="text"
+          placeholder="Filtrar por título o autor"
+          className="filter-input"
+          value={filterText}
+          onChange={handleFilterChange}
         />
-       
-        )
+        
+      </div>
+    <div className="card-container">
+      {filteredPodcasts.map((podcast, index) => {
+        return (
+          <PodcastCard
+            key={index}
+            image={podcast["im:image"][0].label}
+            title={podcast.title.label}
+            author={podcast["im:artist"].label}
+          />
+        );
       })}
-      
+    </div>
     </div>
   );
 }
