8758abd08a0c679c571b25a74631983332414b13
Cesar Garabito <cesargarabito97@gmail.com>
2023-06-29 23:16:37 +0200

    episodedetails done

--- src/components/EpisodeView/EpisodeView.js
+++ src/components/EpisodeView/EpisodeView.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from "react";
+import { PodcastsContext } from "../Contexts/PodcastContext";
+import ReactPlayer from 'react-player';
 
 const EpisodeView = () => {
-  return (
-    <div>EpisodeView</div>
-  )
-}
+  const { selectedEpisodes } = useContext(PodcastsContext);
 
-export default EpisodeView
+  return (
+    <div>
+      <h2>{selectedEpisodes.title}</h2>
+      <p>{selectedEpisodes.description}</p>
+      <ReactPlayer
+        url={selectedEpisodes.episodeUrl}
+        controls
+      />
+    </div>
+  );
+};
+
+export default EpisodeView;
--- src/components/EpisodesSection/EpisodeSection.js
+++ src/components/EpisodesSection/EpisodeSection.js
@@ -1,15 +1,16 @@
-
-import React from 'react';
+import React from "react";
 import { useContext } from "react";
 import { PodcastsContext } from "../Contexts/PodcastContext";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const EpisodeSection = ({ episodes }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { setSelectedEpisodes, selectedEpisodes } = useContext(PodcastsContext);
   const handleSelectedEpisode = (selectedEpisode) => {
     setSelectedEpisodes(selectedEpisode);
-    navigate(`/podcast/${selectedEpisode.podcastId}/episode/${selectedEpisode.id}`)
+    navigate(
+      `/podcast/${selectedEpisode.podcastId}/episode/${selectedEpisode.id}`
+    );
   };
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -23,38 +24,68 @@ const EpisodeSection = ({ episodes }) => {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   };
   return (
-    <div style={{ textAlign: 'left' }}>
-     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <h2>Episodios: </h2>
-      <div style={{ backgroundColor: 'navy', color: 'white', borderRadius: '4px', padding: '8px' }}>{episodes.length}</div>
-    </div>
+    <div style={{ textAlign: "left" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h2>Episodios: </h2>
+        <div
+          style={{
+            backgroundColor: "navy",
+            color: "white",
+            borderRadius: "4px",
+            padding: "8px",
+          }}
+        >
+          {episodes.length}
+        </div>
+      </div>
       <div>
-      <table style={{ borderCollapse: 'collapse', overflowX: 'auto' }}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th style={{ marginRight: '20px'}}>Date</th>
-            <th>Duration</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-        {episodes.map((episode, index) => (
-            <tr key={index} style={{ backgroundColor: index % 2 === 0 ? 'rgba(128, 128, 128, 0.2)' : 'white' }}>
-              <td onClick={() => {handleSelectedEpisode({ podcastId: episode.collectionId, id: episode.trackId
-})}} style={{ color: 'navy' }}>{episode.trackName}</td>
-              <td style={{ marginRight: '20px'}}>{formatDate(episode.releaseDate)}</td>
-              <td>{formatDuration(episode.trackTimeMillis)}</td>
+        <table style={{ borderCollapse: "collapse", overflowX: "auto" }}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th style={{ marginRight: "20px" }}>Date</th>
+              <th>Duration</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {episodes.map((episode, index) => (
+              <tr
+                key={index}
+                style={{
+                  backgroundColor:
+                    index % 2 === 0 ? "rgba(128, 128, 128, 0.2)" : "white",
+                }}
+              >
+                <td
+                  onClick={() => {
+                    handleSelectedEpisode({
+                      podcastId: episode.collectionId,
+                      id: episode.trackId,
+                      title: episode.trackName,
+                      description: episode.description,
+                      episodeUrl: episode.episodeUrl
+                    });
+                  }}
+                  style={{ color: "navy" }}
+                >
+                  {episode.trackName}
+                </td>
+                <td style={{ marginRight: "20px" }}>
+                  {formatDate(episode.releaseDate)}
+                </td>
+                <td>{formatDuration(episode.trackTimeMillis)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
-}
+};
 
 export default EpisodeSection;
