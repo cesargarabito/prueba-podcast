5b01daecb2b925d7028cab9b6b48ac29093ec2de
Cesar Garabito <cesargarabito97@gmail.com>
2023-06-29 23:41:41 +0200

    episodedetail done

--- src/components/EpisodeDetail/EpisodeDetail.js
+++ src/components/EpisodeDetail/EpisodeDetail.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import SideBar from '../SideBar/SideBar'
 import EpisodeView from '../EpisodeView/EpisodeView'
+import Header from '../Header'
 
 const EpisodeDetail = () => {
   return (
     <div>
+        <Header />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <SideBar />
         <EpisodeView />
+        </div>
     </div>
+    
   )
 }
 
--- src/components/EpisodeView/EpisodeView.js
+++ src/components/EpisodeView/EpisodeView.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { useContext } from "react";
 import { PodcastsContext } from "../Contexts/PodcastContext";
 import ReactPlayer from 'react-player';
+import './EpisodeView.css';
 
 const EpisodeView = () => {
   const { selectedEpisodes } = useContext(PodcastsContext);
 
   return (
-    <div>
-      <h2>{selectedEpisodes.title}</h2>
-      <p>{selectedEpisodes.description}</p>
-      <ReactPlayer
-        url={selectedEpisodes.episodeUrl}
-        controls
-      />
+    <div className="episode-container">
+      <h2 className="episode-title">{selectedEpisodes.title}</h2>
+      <p className="episode-description">{selectedEpisodes.description}</p>
+      <div className="react-player-wrapper">
+        <ReactPlayer
+          url={selectedEpisodes.episodeUrl}
+          controls
+          
+        />
+      </div>
     </div>
   );
 };
 
 export default EpisodeView;
+
